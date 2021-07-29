046f336b5e68fc40b19363b99610610459ea832d
grogg19 <differ@list.ru>
2021-07-29 17:32:47 +0300

    добавлен список шагов

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -9,6 +9,14 @@
     <h1>{{ $task->title }}</h1>
     <p class="blog-post-meta">{{ $task->created_at->toFormattedDateString() }}</p>
     {{ $task->body }}
+
+    @if($task->steps->isNotEmpty())
+    <ul class="list-group mt-3">
+        @foreach($task->steps as $step)
+            <li class="list-group-item">{{ $step->description }}</li>
+        @endforeach
+    </ul>
+    @endif
     <div class="mt-4">
         <a class="btn btn-primary" href="{{ route('tasks.edit', $task) }}">Изменить <i class="fas fa-edit"></i></a>
     </div>
