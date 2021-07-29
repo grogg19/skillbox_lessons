eb500bfa14430f5f6d4d4e4391545cd7a07b4fb5
grogg19 <differ@list.ru>
2021-07-29 19:41:32 +0300

    добавлен метод update()

--- app/Http/Controllers/TaskStepsController.php
+++ app/Http/Controllers/TaskStepsController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Step;
 use Illuminate\Http\Request;
 
 class TaskStepsController extends Controller
 {
-    //
+    public function update(Request $request, Step $step)
+    {
+        $step->update(['completed' => $request->boolean('completed')]);
+
+        return redirect(route('tasks.show', ['task' => $step->task->id]));
+    }
 }
