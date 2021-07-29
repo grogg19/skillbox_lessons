9e4a6bd9837725b2dd08c40ad31cad4d0d707d83
grogg19 <differ@list.ru>
2021-07-29 15:53:58 +0300

    маршруты объединены в ресурс

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+
 use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TasksController::class, 'index'])->name('page.main');
-Route::get('/tasks/create', 'TasksController@create')->name('task.create');
-Route::post('/tasks/store', 'TasksController@store')->name('task.save');
-Route::get('/tasks/{id}', 'TasksController@show')->name('task.show');
+
+Route::resource('tasks', 'TasksController');
+
 
 Route::get('/about/', function () {
     return view('about');
