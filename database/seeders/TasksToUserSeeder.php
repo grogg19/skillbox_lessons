c09572d7b930041debff47a0102830998ec03971
grogg19 <differ@list.ru>
2021-08-16 18:55:07 +0300

    новый сидер

---
+++ database/seeders/TasksToUserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Step;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class TasksToUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::factory([
+            'name' => 'Anton Devyatov',
+            'email' => 'differ@list.ru',
+            'password' => Hash::make('12345678')
+        ])->create();
+
+        Task::factory(5)->create([
+            'owner_id' => $user
+        ])->each(function (Task $task) {
+             $task->steps()->saveMany(Step::factory(rand(1,5))->make());
+        });
+    }
+}
